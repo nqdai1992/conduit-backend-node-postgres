529513a3864c1ccd2770c1bd56fc41115dc8ea91
nqdai <nguyendai1992@gmail.com>
2021-07-03 04:52:52 +0700

    Update article entity - Implement Create slug function - Implement merge tag list

--- src/domain/entities/article.entity.ts
+++ src/domain/entities/article.entity.ts
@@ -1,15 +1,18 @@
-import { IUser } from "@/domain/entities/user.entity";
-import { ITag } from "./tag.entity";
-
+export interface IArticeAuthor {
+  username: string,
+  bio: string,
+  image: string,
+  following: string
+}
 export interface IArticle {
   title: string;
   slug: string;
   description: string;
   body: string;
-  tagList: ITag[];
+  tagList: string[];
   favorited: boolean;
   favoritesCount: number;
-  author: Omit<IUser, "password">;
+  author: IArticeAuthor;
   createdAt?: Date;
   updatedAt?: Date;
 }
@@ -19,21 +22,56 @@ export interface ArticleEntity {
   update: (newArticle: Omit<IArticle, "createdAt" | "updatedAt">) => ArticleEntity;
 }
 
-const Article = (initArticle: IArticle): ArticleEntity => ({
-  toObject: () => initArticle,
-  update: (newArticle: Omit<IArticle, "createdAt" | "updatedAt">) => {
-    const { title, description, slug, body, tagList = [], favorited } = newArticle;
-    
-    return Article({
-      ...initArticle,
-      title, 
-      description, 
-      slug, 
-      body, 
-      tagList: [...initArticle.tagList, ...tagList], 
-      favorited
-    })
+//The solution at https://gist.github.com/codeguy/6684588
+const createSlug = (title: string) => {
+  title = title.replace(/^\s+|\s+$/g, ''); // trim
+  title = title.toLowerCase();
+
+  // remove accents, swap ñ for n, etc
+  const from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+  const to   = "aaaaeeeeiiiioooouuuunc------";
+  
+  for (let i = 0; i < from.length; i++) {
+    title = title.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
   }
-})
+
+  title = title.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+    .replace(/\s+/g, '-') // collapse whitespace and replace by -
+    .replace(/-+/g, '-'); // collapse dashes
+
+  return `${title}-${Date.now()}`;
+}
+
+const mergeTagList = (tagList1: string[], tagList2: string[]) => {
+  const convertedTagList1 = tagList1.map(tag => tag.toLowerCase())
+  const convertedTagList2 = tagList2.map(tag => tag.toLowerCase())
+
+  return [...new Set([...convertedTagList1, ...convertedTagList2])]
+}
+
+
+
+const Article = (initArticle: IArticle): ArticleEntity => {
+  initArticle.slug = createSlug(initArticle.title)
+  initArticle.tagList = initArticle.tagList.map(tag => tag.toLowerCase())
+
+  return {
+    toObject: () => initArticle,
+    update: (newArticle: Omit<IArticle, "createdAt" | "updatedAt">) => {
+      const { title, description, slug, body, tagList = [], favorited } = newArticle;
+      const shouldBeRegenerateSlug = title.toLowerCase() !== initArticle.title.toLowerCase()
+      
+      return Article({
+        ...initArticle,
+        title, 
+        description, 
+        slug: shouldBeRegenerateSlug ? createSlug(title) : slug, 
+        body, 
+        tagList: mergeTagList(initArticle.tagList, tagList), 
+        favorited
+      })
+    }
+  }
+}
 
 export default Article;
