7969985d06487dd8ed1e871021cb98cb3cb79003
nqdai <nguyendai1992@gmail.com>
2021-10-09 17:27:18 +0700

    Change user model from functional style to OOP style

--- src/entities/user/User.test.ts
+++ src/entities/user/User.test.ts
@@ -1,74 +1,69 @@
 import { assert } from 'chai'
-import { createUserWithId, createUserWithoutId } from './User'
+import User from './User'
 
 describe('User', () => {
   describe('createUserWithoutId', () => {
-    it('create a new user', () => {
+    it('return a new user has id', () => {
       const email = 'fakeemail@gmail.com'
       const username = 'fakeusername'
 
-      const newUser = createUserWithoutId(username, email)
+      const newUser = User.createUserWithoutId(username, email)
 
-      assert.equal(newUser.getUsername(), username)
-      assert.equal(newUser.getEmail(), email)
-      assert.isNotNull(newUser.getId())
+      assert.isNotNull(newUser.id)
     })
   })
 
   describe('createUserWithId', () => {
-    it('load from existed user', () => {
+    it('return a existed user', () => {
       const email = 'fakeemail@gmail.com'
       const username = 'fakeusername'
       const userId = 'fakeuserid'
 
-      const newUser = createUserWithId(userId, username, email)
-
-      assert.equal(newUser.getUsername(), username)
-      assert.equal(newUser.getEmail(), email)
-      assert.equal(newUser.getId(), userId)
+      const newUser = User.createUserWithId(userId, username, email)
+      
+      assert.equal(newUser.id, userId)
     })
   })
 
   describe('.follow', () => {
     it('save the id of followed user ', () => {
-      const user1 = createUserWithoutId('username1', 'user1@mail.com')
-      const user2 = createUserWithoutId('username2', 'user2@mail.com')
+      const user1 = User.createUserWithoutId('username1', 'user1@mail.com')
+      const user2 = User.createUserWithoutId('username2', 'user2@mail.com')
 
-      assert.include(user1.follow(user2).getFollowsUserIds(), user2.getId())
+      user1.follow(user2)
+
+      assert.include(user1.followedUserIds, user2.id)
     })
 
     it ('can not follow themselves', () => {
-      const user1 = createUserWithoutId('username1', 'user1@mail.com')
+      const user1 = User.createUserWithoutId('username1', 'user1@mail.com')
+      user1.follow(user1)
 
-      assert.notInclude(user1.follow(user1).getFollowsUserIds(), user1.getId())
+      assert.notInclude(user1.followedUserIds, user1.id)
     })
 
     it ('can not follow duplicate user', () => {
-      const user1 = createUserWithoutId('username1', 'user1@mail.com')
-      const user2 = createUserWithoutId('username2', 'user2@mail.com')
+      const user1 = User.createUserWithoutId('username1', 'user1@mail.com')
+      const user2 = User.createUserWithoutId('username2', 'user2@mail.com')
 
-      const followedUserIds = user1.follow(user2).follow(user2).getFollowsUserIds()
+      user1
+      .follow(user2)
+      .follow(user2)
+      
 
-      assert.equal(followedUserIds.filter(id => id === user2.getId()).length, 1)
+      assert.equal(user1.followedUserIds.length, 1)
     })
   })
 
   describe('.unfollow', () => {
     it('remove the id of target user', () => {
-      const user1 = createUserWithoutId('username1', 'user1@mail.com')
-      const user2 = createUserWithoutId('username2', 'user2@mail.com')
-      const followedUserIds = user1.follow(user2).unfollow(user2).getFollowsUserIds()
+      const user1 = User.createUserWithoutId('username1', 'user1@mail.com')
+      const user2 = User.createUserWithoutId('username2', 'user2@mail.com')
+      user1
+      .follow(user2)
+      .unfollow(user2)
 
-      assert.notInclude(followedUserIds, user2.getId())
-    })
-
-    it('can not remove the id that does not existed ', () => {
-      const user1 = createUserWithoutId('username1', 'user1@mail.com')
-      const user2 = createUserWithoutId('username2', 'user2@mail.com')
-
-      const followedUserIds = user1.unfollow(user2).getFollowsUserIds()
-
-      assert.equal(followedUserIds.length, 0)
+      assert.notInclude(user1.followedUserIds, user2.id)
     })
   })
 })
--- src/entities/user/User.ts
+++ src/entities/user/User.ts
@@ -1,43 +1,52 @@
 import { v4 as uuidv4 } from 'uuid'
-
-export interface IUser {
-    getUsername: () => string;
-    getId: () => string;
-    getEmail: () => string;
-    follow: (user: IUser) => IUser;
-    unfollow: (user: IUser) => IUser;
-    getFollowsUserIds: () => string[];
-    toObject: () => { id: string, username: string, email: string }
-}
-
-const User = (id: string, username: string, email: string, followedUserIds = []): IUser => { 
-    return {
-        getUsername: () => username,
-        getId: () => id,
-        getEmail: () => email,
-        follow: (user: IUser) => {
-            if (user.getId() === id) return user
-            
-            const newFollowedUsers = Array.from(new Set([...followedUserIds, user.getId()]))
-            
-            return User(id, username, email, newFollowedUsers)
-        },
-        unfollow: (user: IUser) => {
-            const newFollowedUsers = followedUserIds.filter(id => id !== user.getId())
-            
-            return User(id, username, email, newFollowedUsers)
-        },
-        getFollowsUserIds: () => followedUserIds,
-        toObject: () => ({id, username, email})
+export default class User {
+    #id: string;
+    #username: string;
+    #email: string;
+    #followedUserIds: string[]
+    constructor (id: string, username: string, email: string, followedUserIds?: string[]) {
+        this.#id = id;
+        this.#email = email
+        this.#username = username
+        this.#followedUserIds = followedUserIds || []
     }
-}
+    
+    static createUserWithoutId = (username: string, email: string): User => {
+        return new User(uuidv4(), username, email)
+    }
 
-export const createUserWithoutId = (username: string, email: string): IUser => {
-    return User(uuidv4(), username, email)
-}
+    static createUserWithId = (id: string, username: string, email: string): User => {
+        return new User(id, username, email)
+    }
 
-export const createUserWithId = (id: string, username: string, email: string): IUser => {
-    return User(id, username, email)
-}
+    get id (): string {
+        return this.#id
+    }
 
-export default User;
+    get username (): string {
+        return this.#username
+    }
+
+    get email (): string {
+        return this.#email
+    }
+
+    get followedUserIds (): string[] {
+        return this.#followedUserIds
+    }
+
+    follow (user: User): User {
+        if (this.#id === user.id) return this
+        if (this.#followedUserIds.includes(user.id)) return this
+    
+        this.#followedUserIds.push(user.id)
+
+        return this
+    }
+
+    unfollow (user: User): User {
+        this.#followedUserIds = this.#followedUserIds.filter(id => id !== user.id)
+
+        return this
+    }
+};
